d555d115b6083f92c9f47e9050eb00cf32770789
Abrahantes <marcial1234@ufl.edu>
2016-12-05 16:15:18 -0500

    finished modified encryption and decryption

--- client.py
+++ client.py
@@ -1,78 +1,124 @@
 #!/usr/bin/python
 import os, sys, socket, time
 from random import randint
+from socket import *
 
 # Bases for generators of up to 29:
 # 2, 3, 8, 10, 11, 14, 15, 18, 19, 21, 26, 27
 # Up to 23:
 # 5, 7, 10, 11, 14, 15, 17, 19, 20, 21,
 def get_offset(n, base=19):
-	return base**n % base
+    return base**n % base
 
 # returns an 'n' sized string to be appened to plaintext
 def pad(n):
-	return [chr(randint(0,28)+97) for x in xrange(n)]
-	# return "".join()
+    # Explination: this is a list comprehension.
+    # The loop is ran, and each element inside is what is returned by what's before 'for'
+    return [chr(randint(0,28)+97) for x in xrange(n)]
 
-# TBD
-def scramble(text, key, offset):
-	pass
+# Scrambles key and plaintext by the offset
+# Returns: list of chars
+def scramble(plaintext, key, offset):
+    ciphertext = []
+    for index in xrange(0, len(plaintext), offset):
+        partition = plaintext[index:index+offset]
+        ciphertext.extend(partition)
+        ciphertext.extend(key[index/offset])
+        # Debug Code
+        # print index, index+offset, oink
+        # print key[index/offset]
+    # Index still on the scope
+    ciphertext.extend(plaintext[index*(offset+1):])
+    return ciphertext
 
+# Unscrambles key and plaintext by the offset
+# Returns: [list of chars, list of chars]
+def unscramble(ciphertext, offset):
+    plaintext = []
+    key = []
 
-# 	Crypto part:
-# 		Encryption:
-# 			Message is padded up to a certain number of characters (max length) with random text
-# 			You create a one time AES key
-# 			You encrypt the Message using AES and the above key
-# 			You create a random character (a-z, {|}~) that maps to 0-28 via ord([actual char]) - 97
-# 			You pass the random character thru one of the base generators for 29 (below) that return the step/increment value
-# 			You scramble the Key within the text in gaps of getoffset(randchar)
-# 			randchar goes in the front of the ciphertext
+    for index in xrange(0, len(ciphertext)-offset+1, offset+1):
+        plaintext.extend(message[index:index+offset])
+        key.append(message[index+offset])
+        # Debug Code
+        # print index, index+offset, message[index:index+offset], message[index+offset]
+
+    # Index still on the scope
+    plaintext.extend(message[index+offset+1:])
+    [plaintext, key]
+
+#   Crypto part:
+#       Encryption:
+#           Message is padded up to a certain number of characters (max length) with random text
+#           You create a one time AES key
+#           You encrypt the Message using AES and the above key
+#           You create a random character (a-z, {|}~) that maps to 0-28 via ord([actual char]) - 97
+#           You pass the random character thru one of the base generators for 29 (below) that return the step/increment value
+#           You scramble the Key within the text in gaps of getoffset(randchar)
+#           randchar goes in the front of the ciphertext
 def encrypt(message):
-	# messagee is turned into a list
-	plaintext = message.split()
+    # messagee is turned into a list
+    plaintext = message.split()
+    ciphertext = []
 
-	# Add Random Padding
-	plaintext.extend(pad(set_size-len(plaintext)))
-	# this turns plaintext into a string
-	plaintext = "".join(plaintext)
+    # Add Random Padding
+    # set_size NOT SET, just placeholder
+    plaintext.extend(pad(set_size-len(plaintext)))
 
-	# everything below doesn't work yet, just a placeholder
-	key = AES.generate_key(128)
-	# Scrambled Eggs Time
-	plaintext = scramble(plaintext, key, offset)
-	output = AES.encrypt(cyphertext, key)
-	return output
+    # this turns plaintext into a string
+    # plaintext = "".join(plaintext)
+
+    # everything below doesn't work yet, just a placeholder
+    key = AES.generate_key(128)
+    # Scrambled Eggs Time
+    cyphertext = "".join(scramble(plaintext, key, offset))
+    output = AES.encrypt(cyphertext, key)
+    return output
 
 # Decrypt:
 # Delete first char, and pass it thru getoffset. Use result to build key and message
 # AES Decrypt
 def decrypt(message):
-	# if the message received it's not the specific size/length, return and print nothing
-	if len(message) != size:
-		return ""
-	# IS IT QUARANTEED THAT THIS WILL HAPPEN ALL THE TIME?
-	# NEED TO TEST AES RETURN SIZE GIVEN MOST INPUTS
+    # if the message received it's not the specific size/length, return and print nothing
+    if len(message) != size:
+        return ""
+    # IS IT QUARANTEED THAT THIS WILL HAPPEN ALL THE TIME??
+    # NEED TO TEST AES RETURN SIZE GIVEN MANY INPUTS
 
-	# DO THE THING
-	random_char = ord(message[0] - 97)
-	offset = getoffset(random_char)
-	# skipping the random_char
-	ciphertext = message[1:] 
+    # DO THE THING
+    random_char = ord(message[0]) - 97
+    offset = get_offset(random_char)
 
-	# Unscramble, not done
-	for index in xrange(len(message)-32):
-		pass
+    # skipping the random_char
+    merged_ciphertext = message[1:] 
+    # will be turned into strings later
 
-	# this doesn't work yet, just a placeholder
-	output = AES.decrypt(cyphertext, key)
-	return output
+    # Unscramble, not done
+    plaintext, key = unscramble(merged_ciphertext, offset)
+
+    # this doesn't work yet, just a placeholder
+    plaintext = AES.decrypt(cyphertext, key)
+    return plaintext
 
 # Client code talking to server:
 # https://neerajkhandelwal.wordpress.com/2012/02/16/socket-programming-handling-multiclients/
-
 def main():
-	pass
+    host = 'localhost' # '127.0.0.1' can also be used
+    # port = 52000
+    port = 52725
+    # port = 65535/7
+     
+    sock = socket()
+    # Connecting to socket
+    sock.connect((host, port)) # Connect takes tuple of host and port
+     
+    # Infinite loop to keep client running.
+    while True:
+        data = sock.recv(1024)
+        print data
+        sock.send('HI! I am client.')
+     
+    sock.close()
 
 if __name__ == '__main__':
-	main()
+    main()
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # test this line if you're in Mac/Linux
 
-import os, sys, socket, time
 from uuid import getnode as get_mac
+import os, sys, time #, socket
+from socket import *
 # function for MAC address
 
 # pycrypto Package
@@ -10,72 +11,68 @@ from uuid import getnode as get_mac
 # I'm having issues having issues installing the AES package in UF's computers
 # trying alternatives, like M2Crypto or open sourced vanilla AES implementations
 
-# i wanted to use 67775/1 for something i think
+# i wanted to use 65537/5 for something i think
 
-
-HOST = ''    # All available interfaces
-PORT = 52725 # Arbitrary port
+host = "localhost"
+port =  52725 # Arbitrary port
 
 # Function for handling connections. This will be used to create threads
 def clientthread(client):
     # Sending message to connected client
     client.send('Welcome to the server. Type something and hit enter\n') # send only takes string
     
-    # infinite loop so that function do not terminate and thread do not end.
+    # infinite loop so that function and thread does not terminate
     while True:
-        
         # Receiving from client
         data = client.recv(1024)
         reply = 'OK...' + data
-        if not data: 
-            break
-     
-        client.sendall(reply)
+        client.sendall(data)
     
     client.close()
 
 def main():
-	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print 'Socket created'
-	 
-	# Bind socket to local host and port
-	server.bind((HOST, PORT))
+    # instantiating server socket
+    server = socket(AF_INET, SOCK_STREAM)
+    print 'Socket created'
+     
+    # Bind socket to local host and port
+    server.bind((host, port))
 
-	# Debug Code
-	# try:
-	#     server.bind((HOST, PORT))
-	# except socket.error as msg:
-	#     print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-	#     syserver.exit()
+    # Debug Code
+    # try:
+    #     server.bind((host, port))
+    # except socket.error as msg:
+    #     print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    #     syserver.exit()
 
-	print 'Socket bind complete'
+    print 'Socket bind complete'
 
-	# Start listening on socket
-	# Passed Parameter is # of max connections allowed
-	server.listen(4)
-	print 'Socket now listening'
+    # Start listening on socket
+    # Passed Parameter is # of max connections allowed
+    server.listen(5)
+    print 'Socket now listening'
 
-	basic_key = get_mac()
+    basic_key = get_mac()
 
-	# Stay Connected undefinately
-	# TODO: How to kill it??
-	while True:
-	    # wait to accept a connection - BLOCKING call
-	    client, (ip,port) = server.accept()
-	    start_new_thread(clientthread, (client,))
-	    print 'Connected with ' + ip + ':' + str(port)
+    # Stays Connected undefinately
+    # TODO: How to kill it??
+    while True:
+        # wait to accept a connection - BLOCKING call
+        client, (ip,port) = server.accept()
+        start_new_thread(clientthread, (client,))
+        print 'Connected with ' + ip + ':' + str(port)
 
-	    addr = str(basic_key)
-	    # this list comp formats the MAC Address from a Long to "XX:XX:.."
-	    addr = ':'.join(addr[i:i+2] for i in range(0,12,2))
-	    # send server mac address to client as a test
-	    client.send(addr)
+        addr = str(basic_key)
+        # this list comp formats the MAC Address from a Long to "XX:XX:.."
+        addr = ':'.join(addr[i:i+2] for i in range(0,12,2))
+        # send server mac address to client as a test
+        client.send(addr)
 
-	    # time.sleep(10)
-	    # exits after one, still figuring out to how gracefully exit
-	    break
+        # time.sleep(10)
+        # exits after one, still figuring out to how gracefully exit
+        break
 
-	server.close()
+    server.close()
 
 if __name__ == '__main__':
-	main()
+    main()
